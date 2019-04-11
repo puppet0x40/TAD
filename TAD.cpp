9040e260a4e21a9cb7f16723296f28f5289c0f8b
puppet0x40 <https://github.com/puppet0x40/TAD.git>
2019-04-11 14:23:17 -0300

    Update TAD.cpp

--- TAD.cpp
+++ TAD.cpp
@@ -3,10 +3,8 @@
 #include <cstdio>
 #include <clocale>
 #define cls system("cls");  //limpa tela
-#define pausa getchar();getchar()
-//#define pausa system("pause") //
+#define pausa getchar();getchar() //#define pausa system("pause");
 const int TAMANHO = 20; // fixa o tamnho do vetor e da condicional do for
-
 using namespace std;
 
 /// prototipos
@@ -30,6 +28,7 @@ int lista[TAMANHO];
 int main(){
    int opcao;
    setlocale(LC_ALL,"portuguese");
+   system("color 2"); // setar cor de o texto e fundo
    /** zona de teste ** zona de teste ** zona de teste ***/
    //for(int i=0; i<TAMANHO; i++){lista[i] = 1;}
    lista[5] = 987;
@@ -39,39 +38,36 @@ int main(){
    lista[13] = 0;
    lista[15] = 9999;
    /** zona de teste ** zona de teste ** zona de teste ***/
-   while(true){
-      do{
-         cls;
-         cout << "\t\tTAD\t\t"<< quantidadeOcupado(lista) << "/"<<TAMANHO<<"\n" <<
-                 "\t1. Mostrar lista\n" <<
-                 "\t2. *Inserir elemento*\n" <<
-                 "\t3. *Remover elemento*\n" <<
-                 "\t4. procura elemento\n" <<
-                 "\t0. sair\n" <<
-                 "\tOpcao: ";
-         cin >> opcao;
-         switch(opcao){
-         case 0:
-            exit(true);
-         case 1:
-            mostrar(lista);
+   do{
+      cls;
+      cout << "\t\tTAD\t\t"<< quantidadeOcupado(lista) << "/"<<TAMANHO<<"\n" <<
+              "\t1. Mostrar lista\n" <<
+              "\t2. *Inserir elemento*\n" <<
+              "\t3. *Remover elemento*\n" <<
+              "\t4. procura elemento\n" <<
+              "\t0. sair\n" <<
+              "\tOpcao: ";
+      cin >> opcao;
+      switch(opcao){
+      case 0:
+         exit(true);
+      case 1:
+         mostrar(lista);
          break;
-         case 2:
-            adicionar(lista);
-            break;
-         case 3:
-            //excluir();
-            break;
-         case 4:
-            pesquisar(lista);
-
-            break;
-         default:
-            cout << "\t\topcao inválida";
-            pausa;
-         }
-      }while(opcao < 0 || opcao > 4);
-   }
+      case 2:
+         adicionar(lista);
+         break;
+      case 3:
+         //excluir();
+         break;
+      case 4:
+         pesquisar(lista);
+         break;
+      default:
+         cout << "\t\topcao inválida";
+         pausa;
+      }
+   }while(true); /// coloque sempre verdade no do-while, pois ja  tenho uma verificaçao interna com switch com exit(true);
 }
 
 
@@ -87,7 +83,7 @@ bool isCheio(int lista[]){ //ok
   return true;
 }
 
-bool isVazia(int lista[]){ //ok
+bool isVazia(int lista[]){ //ok otimizado
    for(int i =0; i<TAMANHO; i++){
       if(lista[i] != 0){
          return false;
@@ -96,7 +92,7 @@ bool isVazia(int lista[]){ //ok
    return true;
 }
 
-int quantidadeOcupado(int lista[]){ //ok
+int quantidadeOcupado(int lista[]){ //ok otimizado
    int contador = 0;
    for(int i=0; i<TAMANHO; i++){
       if(lista[i] != 0){
@@ -124,14 +120,35 @@ int procurar(int elemento, int lista[]){ //ok
    return -1;
 }
 
-
-void inserir(int indice, int elemento, int listra[]){ //
+//!                 em desenvolvimento                        !//
+void inserir(int indice, int elemento, int lista[]){ //
+   int proximoValor,AnteriorValor,antigoValor,aux;
    if(isCheio(lista)){
       cout << "lista cheia!";
       pausa;
    }else{
-      while(indice<TAMANHO){
+      if(indice == 0){                                /// se for no inicio da lista
+         if(lista[0] == 0){                           /// verifica se primeiro indice está vazio
+            lista[0] = elemento;                      /// se estive coloque o valor no inicio
+         }else{                                       /// se nao tive
 
+            for(int i = 0; i<TAMANHO; i++){           /// faça um varredura e
+               if(lista[i+1] == 0){                   /// vificique se proximo está vazio
+                  antigoValor = lista[i];            /// se estive vazio proximo pega o primeiro valor
+                  lista[i] = elemento;                /// na pociçao do primeiro coloque novo valor
+                  lista[i+1] = antigoValor;          /// coloque o antigo valor do primeiro
+                  break;
+               }/*else{
+                  aux = lista[i+1];
+                  lista[i+1] = lista[i];
+                  lista[i] = aux;
+
+               }*/
+
+            }
+         }
+      }else if(indice == TAMANHO-1){
+         //
       }
    }
 
